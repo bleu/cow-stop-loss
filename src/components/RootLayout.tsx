eb8f4e12cee6504ec1d3b5ffb56494cb083629d1
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-02-09 11:28:19 -0300

    chore: change bg color on SafeLoader

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -22,7 +22,7 @@ export function RootLayout({ children }: React.PropsWithChildren) {
 
 function SafeLoader() {
   return (
-    <div className="flex size-full flex-col justify-center  items-center rounded-3xl px-12 py-16 md:py-20 text-slate12">
+    <div className="bg-blue1 flex size-full flex-col justify-center items-center px-12 py-16 md:py-20 text-slate12">
       <div className="text-center text-3xl">This is a Safe (wallet) App</div>
       <p className="text-xl">
         To access please use this
