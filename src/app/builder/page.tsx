e4d8520149e906bba9c566107856eb604bc57c31
luizakp <luiza.puccinelli06@gmail.com>
2024-02-27 15:44:38 -0300

    chore: create header

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -4,15 +4,10 @@ import { ReactFlowProvider } from "reactflow";
 
 import { Board } from "#/components/Board";
 import Menu from "#/components/menus";
-import { Separator } from "#/components/ui/separator";
 
 export default function PlaygroundPage() {
   return (
     <div className="hidden h-full flex-col md:flex text-white">
-      <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-        <h2 className="text-lg font-semibold">Stop Loss</h2>
-      </div>
-      <Separator />
       <ReactFlowProvider>
         <div className="container size-full py-6">
           <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_300px]">
---
+++ src/app/history/page.tsx
@@ -0,0 +1,9 @@
+export default function HistoryPage() {
+  return (
+    <div className="hidden h-full flex-col md:flex text-white">
+      <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+        <h2 className="text-lg font-semibold">History</h2>
+      </div>
+    </div>
+  );
+}
