9d42e7161c5e662ee18d0bf8d0f893ca03f0668a
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-03-06 14:22:22 -0300

    chore: add tooltip on checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,15 +1,27 @@
+import { slateDarkA } from "@radix-ui/colors";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
+import { CheckIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 import React from "react";
+
+import { Tooltip } from "./Tooltip";
 
 interface ICheckbox {
   id: string;
   checked: boolean;
   onChange: () => void;
   label: string;
+  tooltipText?: string;
+  tooltipLink?: string;
 }
 
-export function Checkbox({ id, checked, onChange, label }: ICheckbox) {
+export function Checkbox({
+  id,
+  checked,
+  onChange,
+  label,
+  tooltipLink,
+  tooltipText,
+}: ICheckbox) {
   return (
     <div className="flex items-center">
       <CheckboxPrimitive.Root
@@ -25,6 +37,17 @@ export function Checkbox({ id, checked, onChange, label }: ICheckbox) {
       <label htmlFor={id} className="pl-[15px] text-[15px] leading-8">
         {label}
       </label>
+      {tooltipText && (
+        <Tooltip content={tooltipText}>
+          {tooltipLink ? (
+            <a href={tooltipLink} target="_blank">
+              <InfoCircledIcon color={slateDarkA.slateA11} />
+            </a>
+          ) : (
+            <InfoCircledIcon color={slateDarkA.slateA11} />
+          )}
+        </Tooltip>
+      )}
     </div>
   );
 }
