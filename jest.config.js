6d89ba59794c13771480a32579ad3fb1b3d5112f
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-03-07 17:31:03 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,6 @@
 /* eslint-env node */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
   preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "node",
 };
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
