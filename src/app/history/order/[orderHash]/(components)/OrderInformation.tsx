33e3e2e8eeac7764a235860da7b8f6b89d68ded1
luizakp <luiza.puccinelli06@gmail.com>
2024-03-27 16:24:51 -0300

    chore: add tooltip on orders details

--- src/app/history/order/[orderHash]/(components)/OrderInformation.tsx
+++ src/app/history/order/[orderHash]/(components)/OrderInformation.tsx
@@ -1,13 +1,31 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "@bleu-fi/ui";
+import {  QuestionMarkCircledIcon } from "@radix-ui/react-icons";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+
 export function OrderInformation ({
   label,
-  children
+  children,
+  tooltipText
 }: {
   label: string;
   children: React.ReactNode;
+  tooltipText?: string;
 }){
   return (
-    <div className="flex">
-      <span className="font-bold">{label}:</span> {children}
-    </div>
+    <TooltipProvider>
+      <div className="flex items-center gap-x-2">
+        {tooltipText && (
+            <Tooltip>
+              <TooltipContent>
+                {tooltipText}
+              </TooltipContent>
+              <TooltipTrigger disabled>
+                <QuestionMarkCircledIcon />
+              </TooltipTrigger>
+            </Tooltip>
+          )}
+          <span className="font-bold">{label}:</span> {children}
+      </div>
+    </TooltipProvider>
   )
 }
