e12603a3b31252d0a621a4c0071835640ffc0ada
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-03-27 16:56:02 -0300

    chore: add warning on sell amount bigger than wallet amount

--- src/components/nodes/SwapNode.tsx
+++ src/components/nodes/SwapNode.tsx
@@ -1,5 +1,13 @@
-import { ISwapData } from "#/lib/types";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { formatUnits } from "viem";
 
+import { useRecipeData } from "#/hooks/useRecipeData";
+import { useSafeBalances } from "#/hooks/useSafeBalances";
+import { calculateAmounts } from "#/lib/calculateAmounts";
+import { ISwapData } from "#/lib/types";
+import { formatNumber } from "#/utils";
+
+import { Tooltip } from "../Tooltip";
 import { BaseNode } from ".";
 
 export function SwapNode({
@@ -9,16 +17,36 @@ export function SwapNode({
   selected: boolean;
   data: ISwapData;
 }) {
+  const { fetchBalance } = useSafeBalances();
+  const { getOrderDataByOrderId } = useRecipeData();
+  const recipeData = getOrderDataByOrderId(data.orderId);
+  if (!recipeData) return null;
+  const sellTokenWalletAmount = Number(
+    formatUnits(
+      BigInt(fetchBalance(data.tokenSell.address)),
+      data.tokenSell.decimals
+    )
+  );
+  const [sellAmount, buyAmount] = calculateAmounts(recipeData);
+  const sellAmountWithSymbol = `${formatNumber(sellAmount, 2, "decimal", "compact", 0.01)} ${data.tokenSell.symbol}`;
+  const buyAmountWithSymbol = `${formatNumber(buyAmount, 2, "decimal", "compact", 0.01)} ${data.tokenBuy.symbol}`;
   return (
     <BaseNode selected={selected}>
-      <div className="flex">
-        <div className="ml-2">
-          <div className="text-sm font-bold text-highlight">Swap</div>
-          <div className="text-xs">
-            {`${data.isSellOrder ? "Sell" : "Buy"} ${data.amount} ${
-              data.tokenSell.symbol
-            } for ${data.tokenBuy.symbol}`}
-          </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row gap-2 items-center">
+          <span className="text-sm font-bold text-highlight">Swap</span>
+          {sellTokenWalletAmount < sellAmount && (
+            <a>
+              <Tooltip content="You don't have enough amount of the selling token. The order can still be posted but it just can be filled when you have the tokens.">
+                <ExclamationTriangleIcon className="size-3 text-warning text-bold" />
+              </Tooltip>
+            </a>
+          )}
+        </div>{" "}
+        <div className="text-xs">
+          {data.isSellOrder
+            ? `Sell ${sellAmountWithSymbol} for at least ${buyAmountWithSymbol}`
+            : `Buy ${buyAmountWithSymbol} with at most ${sellAmountWithSymbol}`}
         </div>
       </div>
     </BaseNode>
