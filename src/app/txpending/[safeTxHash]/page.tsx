678063787a6d1d806b30f149227a0dfa1c626741
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-03-27 14:18:38 -0300

    add tx pending page

---
+++ src/app/txpending/[safeTxHash]/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
+import { TransactionStatus } from "@safe-global/safe-apps-sdk";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { Spinner } from "#/components/Spinner";
+
+export default function Page({
+  params: { safeTxHash },
+}: {
+  params: { safeTxHash: string };
+}) {
+  const { sdk } = useSafeAppsSDK();
+  const router = useRouter();
+
+  async function redirectToHistoryOnTxExecuted() {
+    sdk.txs.getBySafeTxHash(safeTxHash).then((tx) => {
+      if (tx.txStatus === TransactionStatus.SUCCESS) {
+        // Wait 1 second to wait subgraph to index the transaction
+        setTimeout(() => {
+          router.push("/history");
+        }, 1000);
+      }
+    });
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(redirectToHistoryOnTxExecuted, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="flex size-full flex-col items-center px-12 py-16 md:py-20 bg-background">
+      <div className="text-center text-3xl text-foreground mb-4">
+        The transaction is being processed
+      </div>
+      <Spinner size="lg" />
+    </div>
+  );
+}
