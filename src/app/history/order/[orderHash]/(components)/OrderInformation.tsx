18465a14278c68810f5524ea862e91379075458a
luizakp <luiza.puccinelli06@gmail.com>
2024-03-28 10:32:28 -0300

    chore: add grid on order info

--- src/app/history/order/[orderHash]/(components)/OrderInformation.tsx
+++ src/app/history/order/[orderHash]/(components)/OrderInformation.tsx
@@ -2,6 +2,21 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@bleu-fi/ui";
 import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 
+const InfoTooltip = ({ text }: { text?: string }) => {
+  if (!text) return null;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger disabled>
+        <QuestionMarkCircledIcon />
+      </TooltipTrigger>
+      <TooltipContent className="max-w-56 text-center">
+        {text}
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
 export function OrderInformation({
   label,
   children,
@@ -13,16 +28,14 @@ export function OrderInformation({
 }) {
   return (
     <TooltipProvider>
-      <div className="flex items-center gap-x-2">
-        {tooltipText && (
-          <Tooltip>
-            <TooltipContent>{tooltipText}</TooltipContent>
-            <TooltipTrigger disabled>
-              <QuestionMarkCircledIcon />
-            </TooltipTrigger>
-          </Tooltip>
-        )}
-        <span className="font-bold">{label}:</span> {children}
+      <div className="gap-x-2 grid grid-cols-5">
+        <div className="flex items-center gap-x-1 col-span-1">
+          <InfoTooltip text={tooltipText} />
+          <span className="font-bold">{label}:</span>
+        </div>
+        <span className="col-span-4">
+          {children}
+        </span>
       </div>
     </TooltipProvider>
   );
