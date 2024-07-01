a4aea82d5f80b3657f2e8a8bdca66259d338dec3
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-07-01 17:44:31 -0300

    remove unused files

--- src/lib/gql/client.ts
+++ src/lib/gql/client.ts
@@ -1,18 +1,11 @@
 import { ChainId } from "#/lib/publicClients";
 import { SUBGRAPHS, Subgraph } from "codegen";
 import { GraphQLClient } from "graphql-request";
-import balancerGaugesSdk from "./balancer-gauges";
 import composableCowSdk from "./composable-cow";
 
 const clientFor = (client: Subgraph) => (chainId: ChainId) => {
   const endpoint = SUBGRAPHS[client].endpointFor(chainId);
   return new GraphQLClient(endpoint);
-};
-
-export const balancerGaugesApi = {
-  client: clientFor(Subgraph.BalancerGauges),
-  gql: (chainId: ChainId) =>
-    balancerGaugesSdk[chainId](balancerGaugesApi.client(chainId)),
 };
 
 export const composableCowApi = {
