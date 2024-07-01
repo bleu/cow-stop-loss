52429f139a44455b2187b481ca5a2428448c61e2
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-07-01 16:09:30 -0300

    refactor swap card width

--- src/app/refactor/page.tsx
+++ src/app/refactor/page.tsx
@@ -5,12 +5,12 @@ import { SwapCardContextProvider } from "#/contexts/swapCardContext";
 export default function Page() {
   return (
     <div className="size-full py-5 px-8 flex gap-6">
-      <div className="flex h-full w-2/5">
+      <div className="flex w-[600px]">
         <SwapCardContextProvider>
           <SwapCard />
         </SwapCardContextProvider>
       </div>
-      <div className="h-full w-3/5">
+      <div className="size-full">
         <OrderTabs />
       </div>
     </div>
