a8bf4e8888296a94b79aff61c0d9f556807d5eb2
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-07-03 11:01:13 -0300

    add swap card introduction

--- src/components/SwapCard.tsx
+++ src/components/SwapCard.tsx
@@ -1,77 +1,14 @@
 "use client";
 
-import { Card, CardContent, CardTitle } from "@bleu/ui";
-import { zodResolver } from "@hookform/resolvers/zod/dist/zod";
-import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
-import { useForm } from "react-hook-form";
-
 import { useSwapCardContext } from "#/contexts/swapCardContext";
-import { ChainId } from "#/lib/publicClients";
-import { generateSwapSchema } from "#/lib/schema";
-import { SwapData } from "#/lib/types";
 
-import { AdvancedSettingsAlert } from "./AdvancedSettingsAlert";
-import { AdvancedSettingsDialog } from "./AdvancedSettingsDialog";
-import { InvertTokensSeparator } from "./InvertTokensSeparator";
-import { OrderTypeSwitch } from "./OrderTypeSwitch";
-import { PriceInputCard } from "./PriceInputCard";
-import { ReviewOrdersDialog } from "./ReviewOrdersDialog";
-import { SwapCardSubmitButton } from "./SwapCardSubmitButton";
-import { TokenInputCard } from "./TokenInputCard";
-import { Form } from "./ui/form";
+import { SwapCardIntroduction } from "./SwapCardIntroduction";
+import { SwapForm } from "./SwapForm";
 
 export function SwapCard() {
-  const {
-    safe: { chainId },
-  } = useSafeAppsSDK();
-  const formSchema = generateSwapSchema(chainId as ChainId);
-  const {
-    createDraftOrder,
-    currentDraftOrder,
-    setCurrentDraftOrder,
-    setReviewDialogOpen,
-    reviewDialogOpen,
-  } = useSwapCardContext();
-  const form = useForm<SwapData>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      isSellOrder: true,
-    },
-  });
-
-  return (
-    <Form
-      {...form}
-      onSubmit={(data) => {
-        const newOrder = createDraftOrder(data);
-        setCurrentDraftOrder(newOrder);
-        setReviewDialogOpen(true);
-      }}
-      className="w-full"
-    >
-      <ReviewOrdersDialog
-        setOpen={setReviewDialogOpen}
-        open={reviewDialogOpen}
-        draftOrders={currentDraftOrder ? [currentDraftOrder] : []}
-        showAddOrders
-      />
-      <Card className="bg-foreground text-background w-full p-5 rounded-md overflow-auto">
-        <CardTitle className="w-full flex justify-between">
-          <OrderTypeSwitch />
-          <AdvancedSettingsDialog />
-        </CardTitle>
-        <CardContent className="flex flex-col gap-4 py-5 px-0">
-          <TokenInputCard side="Sell" />
-          <div className="flex gap-2 justify-between">
-            <PriceInputCard fieldName="strikePrice" showMarketPrice />
-            <PriceInputCard fieldName="limitPrice" />
-          </div>
-          <InvertTokensSeparator />
-          <TokenInputCard side="Buy" />
-          <AdvancedSettingsAlert />
-          <SwapCardSubmitButton />
-        </CardContent>
-      </Card>
-    </Form>
-  );
+  const { firstAccess } = useSwapCardContext();
+  if (firstAccess) {
+    return <SwapCardIntroduction />;
+  }
+  return <SwapForm />;
 }
