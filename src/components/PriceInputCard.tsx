2039e9fc434f4cdbdcbc98caf393b1c4b667e29c
José Ribeiro <me@joseribeiro.dev>
2024-07-12 00:45:48 -0300

    allow user to invert token price denominator when setting strike and limit prices
    close COW-317

--- src/components/PriceInputCard.tsx
+++ src/components/PriceInputCard.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, CardContent, CardTitle, Input } from "@bleu/ui";
-import { memo, useEffect } from "react";
+import { memo, useEffect, useState } from "react";
 import { useFormContext, useWatch } from "react-hook-form";
 
 import { calculateAmounts } from "#/lib/calculateAmounts";
@@ -22,10 +22,17 @@ function PriceInputCardComponent({
   const { register, control, getValues, setValue } = useFormContext<SwapData>();
   const title = fieldName === "limitPrice" ? "Limit price" : "Trigger price";
 
-  const [tokenBuy, price, marketPrice] = useWatch({
+  const [tokenBuy, tokenSell, formPrice, marketPrice] = useWatch({
     control,
-    name: ["tokenBuy", fieldName, "marketPrice"],
+    name: ["tokenBuy", "tokenSell", fieldName, "marketPrice"],
   });
+
+  const [isInverted, setIsInverted] = useState(false);
+  const [displayPrice, setDisplayPrice] = useState(formPrice);
+
+  useEffect(() => {
+    updateDisplayPrice(formPrice);
+  }, [formPrice, isInverted]);
 
   async function updateDisabledAmount() {
     const isSellOrder = getValues("isSellOrder");
@@ -34,7 +41,7 @@ function PriceInputCardComponent({
     const [sellAmount, buyAmount] = calculateAmounts({
       isSellOrder,
       amount: referenceAmount,
-      limitPrice: price,
+      limitPrice: formPrice,
     });
     setValue(
       isSellOrder ? "amountBuy" : "amountSell",
@@ -46,15 +53,37 @@ function PriceInputCardComponent({
     if (fieldName === "limitPrice") {
       updateDisabledAmount();
     }
-  }, [price]);
+  }, [formPrice]);
 
   const tooltipText =
     fieldName === "limitPrice"
       ? TOOLTIP_DESCRIPTIONS.LIMIT_PRICE
       : TOOLTIP_DESCRIPTIONS.TRIGGER_PRICE;
 
+  const handleInvert = () => {
+    setIsInverted(!isInverted);
+    updateDisplayPrice(formPrice);
+  };
+
+  const updateDisplayPrice = (price: number | undefined) => {
+    if (price !== undefined && price !== 0) {
+      setDisplayPrice(isInverted ? 1 / price : price);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = parseFloat(e.target.value);
+    if (!isNaN(inputValue) && inputValue !== 0) {
+      const newFormPrice = isInverted ? 1 / inputValue : inputValue;
+      setValue(fieldName, newFormPrice);
+      setDisplayPrice(inputValue);
+    } else {
+      setDisplayPrice(undefined);
+    }
+  };
+
   return (
-    <Card className="bg-background  w-full p-2 rounded-lg">
+    <Card className="bg-background w-full p-2 rounded-lg">
       <CardTitle>
         <div className="flex justify-between font-normal text-xs">
           <div className="flex gap-1">
@@ -64,9 +93,8 @@ function PriceInputCardComponent({
         </div>
       </CardTitle>
       <CardContent className="flex flex-col gap-1 px-0 py-2 items-start">
-        <div className="flex justify-between items-center gap-5">
+        <div className="flex justify-between items-center gap-5 w-full">
           <Input
-            {...register(fieldName)}
             type="number"
             disabled={disabled}
             step={1 / 10 ** 18}
@@ -75,8 +103,21 @@ function PriceInputCardComponent({
             min={0}
             onKeyDown={preventNegativeKeyDown}
             onPaste={pasteAbsoluteValue}
+            value={displayPrice !== undefined ? displayPrice : ""}
+            onChange={handleInputChange}
           />
-          {tokenBuy && tokenBuy && <span>{tokenBuy.symbol}</span>}
+          <input type="hidden" {...register(fieldName)} />
+          {tokenBuy && tokenSell && (
+            <Button
+              type="button"
+              variant="ghost"
+              className="py-0 px-1 h-fit text-accent text-xs text-end"
+              onClick={handleInvert}
+            >
+              {isInverted ? tokenSell.symbol : tokenBuy.symbol}/<br />
+              {isInverted ? tokenBuy.symbol : tokenSell.symbol}
+            </Button>
+          )}
         </div>
         {marketPrice && (
           <Button
@@ -84,7 +125,9 @@ function PriceInputCardComponent({
             variant="ghost"
             className="py-0 px-1 h-fit text-accent text-xs"
             onClick={() => {
-              setValue(fieldName, Number(marketPrice.toFixed(4)));
+              const newPrice = isInverted ? 1 / marketPrice : marketPrice;
+              setValue(fieldName, marketPrice);
+              setDisplayPrice(newPrice);
             }}
           >
             Market
