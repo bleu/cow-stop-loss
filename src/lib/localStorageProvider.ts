774c9da8362591a75a7b473c6dfdc8faf435feb6
José Ribeiro <me@joseribeiro.dev>
2024-07-16 11:54:16 -0300

    fix localstorageProvider not being exported

--- src/lib/localStorageProvider.ts
+++ src/lib/localStorageProvider.ts
@@ -1,10 +1,15 @@
-function localStorageProvider() {
-  const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
+export function localStorageProvider() {
+  // When initializing, we restore the data from `localStorage` into a map.
+  const map = new Map<string, object>(
+    JSON.parse(localStorage.getItem("app-cache") || "[]")
+  );
 
+  // Before unloading the app, we write back all the data into `localStorage`.
   window.addEventListener("beforeunload", () => {
     const appCache = JSON.stringify(Array.from(map.entries()));
     localStorage.setItem("app-cache", appCache);
   });
 
+  // We still use the map for write & read for performance.
   return map;
 }
